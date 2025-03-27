25ba9e29e85f056ee653dabe745e6a99a30ab29c
Blanca-estela-valerio-rivero <valerioblancaestela@gmail.com>
2025-03-27 10:09:58 -0600

    Estructura en el horarioblanca con v-for

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
+import Horarioblanca from './components/Horarioblanca.vue';
 import HelloWorld from './components/HelloWorld.vue'
 import recorrido from './components/recorrido.vue';
 import recorridoblanca from './recorridoblanca.vue';
 import Horario from './components/Horario.vue';
+
 </script>
 
 <template>
-  <horario/>
+  <Horarioblanca/>
+ <!----------- <horario/>---------------> 
   <!-----------------------------<recorrido/>!---------->
  <!--------------<recorridoblanca/> --> 
 <!----HelloWorld msg="Vite + Vue" />!-->
