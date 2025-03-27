171134b22342fa4c9a6511b8b060fb7dfa9571d5
Blanca-estela-valerio-rivero <valerioblancaestela@gmail.com>
2025-03-27 09:34:33 -0600

    estructura de horario

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
 import HelloWorld from './components/HelloWorld.vue'
 import recorrido from './components/recorrido.vue';
 import recorridoblanca from './recorridoblanca.vue';
+import Horario from './components/Horario.vue';
 </script>
 
 <template>
+  <horario/>
   <!-----------------------------<recorrido/>!---------->
-  <recorridoblanca/>
+ <!--------------<recorridoblanca/> --> 
 <!----HelloWorld msg="Vite + Vue" />!-->
+
+
       
 </template>
 
